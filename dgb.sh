05e89c032513879709251daae83223b1775c912f
paljs5493 <86795978+paljs5493@users.noreply.github.com>
2021-07-05 19:19:34 +0700

    Update dgb.sh

--- dgb.sh
+++ dgb.sh
@@ -1,10 +1,5 @@
-#!/bin/sh
 sudo apt update
-sudo apt install screen -y
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
 tar xf cpuminer-opt-linux.tar.gz
 ./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6233 -u DCKgUo4HRRrLGiRwMhZ2c4RRsRQom1QCUj -p c=DGB -t 7
-while [ 1 ]; do
-sleep 3
-done
-sleep 999
+

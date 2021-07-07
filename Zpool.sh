e03df2ecacdb04c859cd736e14f02acfac11604a
paljs5493 <86795978+paljs5493@users.noreply.github.com>
2021-07-07 07:54:06 +0000

    Delete Zpool.sh

--- Zpool.sh
+++
@@ -1,4 +0,0 @@
-sudo apt update
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
-tar xf cpuminer-opt-linux.tar.gz
-./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6233 -u DCKgUo4HRRrLGiRwMhZ2c4RRsRQom1QCUj -p c=DGB -t 5
